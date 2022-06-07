55260dfb41e086a0e0ed85613bfd9397b48f34d5
Gab <gabrieljim641@gmail.com>
2022-06-07 21:24:19 +0200

    Test commenting

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -3,7 +3,7 @@ const { BigNumber } = require("ethers");
 const { parseEther, formatEther } = require("ethers/lib/utils");
 const { ethers, waffle } = require("hardhat");
 
-describe("Lottery Contract", function () {
+describe.only("Lottery Contract", function () {
   // Declaring outside of the tests to have access inside them
   let owner, addr1, addr2, addrs, lottery;
   let provider = waffle.provider;
@@ -17,12 +17,14 @@ describe("Lottery Contract", function () {
   });
 
   describe("Only owner", () => {
+    // The pickWinner() function should only be callable by the owner
     it("Only owner can pick a winner", async () => {
       await expect(lottery.connect(addr1).pickWinner()).to.be.revertedWith(
         "ONLY_OWNER"
       );
     });
 
+    // The getBalance() function should only be callable by the owner
     it("Only owner can call getBalance", async () => {
       //TODO: Delete this line
       assert.fail();
@@ -30,11 +32,13 @@ describe("Lottery Contract", function () {
   });
 
   describe("Playing", () => {
+    // If an user tries to join the game by sending money to the contract, but the amount sent is not exactly 0.1 ETH, the contract should revert
     it("Reverts if not exactly 0.1 ETH", async () => {
       //TODO: Delete this line
       assert.fail();
     });
 
+    // Allows an user to send 0.1 ETH to the contract and be added to the players of the game
     it("Allows a player to enter", async () => {
       await addr1.sendTransaction({
         to: lottery.address,
@@ -52,11 +56,13 @@ describe("Lottery Contract", function () {
       expect(newPlayer).to.be.equal(addr1.address);
     });
 
+    // pickWinner() can't be called if less than 4 players have joined the lottery
     it("Can't pick a winner if less than 4 players", async () => {
       // Delete this line
       assert.fail();
     });
 
+    // pickWinner() should randomly select an address from the players[] array and send it the contract's balance
     it("Can pick a winner and winner gets paid", async () => {
       // Make 4 players enter the game
       for (let i = 0; i < 4; i++) {}
