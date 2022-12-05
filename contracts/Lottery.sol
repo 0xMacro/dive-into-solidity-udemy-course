690a99c4b836841d6e183a0557d602e8168eb519
Sagar Shah <shah.sagar.nilesh@gmail.com>
2022-12-05 14:44:25 +0530

    Completed Lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -11,36 +11,40 @@ contract Lottery {
 
     // declaring the constructor
     constructor() {
-        // TODO: initialize the owner to the address that deploys the contract
+        owner = msg.sender;
     }
 
     // declaring the receive() function that is necessary to receive ETH
     receive() external payable {
         // TODO: require each player to send exactly 0.1 ETH
         // TODO: append the new player to the players array
+        require(msg.value == 0.1 ether, "Amt should be 0.1 ETH");
+        players.push(msg.sender);
     }
 
     // returning the contract's balance in wei
     function getBalance() public view returns (uint256) {
-        // TODO: restrict this function so only the owner is allowed to call it
-        // TODO: return the balance of this address
+        require(msg.sender == owner, "ONLY_OWNER");
+        return address(this).balance;
     }
 
     // selecting the winner
     function pickWinner() public {
-        // TODO: only the owner can pick a winner 
-        // TODO: owner can only pick a winner if there are at least 3 players in the lottery
+        require(msg.sender == owner, "ONLY_OWNER");
+        require(players.length >= 3, "NOT_ENOUGH_PLAYERS");
 
         uint256 r = random();
         address winner;
 
-        // TODO: compute an unsafe random index of the array and assign it to the winner variable 
+        uint256 index = r % players.length;
+        winner = players[index];
 
-        // TODO: append the winner to the gameWinners array
+        gameWinners.push(winner);
 
-        // TODO: reset the lottery for the next round
+        delete players;
 
-        // TODO: transfer the entire contract's balance to the winner
+        (bool result, ) = winner.call{value: getBalance()}("");
+        require(result, "TRANSFER_FAILED");
     }
 
     // helper function that returns a big random integer
